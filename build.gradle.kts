ecb33f76077f9baba4c2b2504700705b2b6e7e82
ewoudje <7384674+ewoudje@users.noreply.github.com>
2024-01-23 23:17:59 +0100

    VS Whoa

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,17 +1,31 @@
 plugins {
     kotlin("multiplatform") version "1.9.0"
     id("io.kotest.multiplatform") version "5.7.2"
+    id("maven-publish")
 }
 
 val kotestVersion = "5.7.2"
 
 group = "com.ewoudje"
-version = "1.0-SNAPSHOT"
-
-
+val gitRevision = "git rev-parse HEAD".execute()
+version = "0.1.0+" + gitRevision.substring(0, 10)
 
 repositories {
     mavenCentral()
+    maven {
+        name = "VS Maven"
+        url = uri(project.findProperty("vs_maven_url") ?: "https://maven.valkyrienskies.org/")
+
+        val vsMavenUsername = project.findProperty("vs_maven_username") as String?
+        val vsMavenPassword = project.findProperty("vs_maven_password") as String?
+
+        if (vsMavenPassword != null && vsMavenUsername != null) {
+            credentials {
+                username = vsMavenUsername
+                password = vsMavenPassword
+            }
+        }
+    }
 }
 
 kotlin {
@@ -44,4 +58,33 @@ kotlin {
 
 tasks.withType<Test>().configureEach {
     useJUnitPlatform()
+}
+
+publishing {
+    repositories {
+        val vsMavenUsername = project.findProperty("vs_maven_username") as String?
+        val vsMavenPassword = project.findProperty("vs_maven_password") as String?
+        val vsMavenUrl = project.findProperty("vs_maven_url") as String?
+        if (vsMavenUrl != null && vsMavenPassword != null && vsMavenUsername != null) {
+            println("Publishing to VS Maven ($version)")
+            maven {
+                url = uri(vsMavenUrl)
+                credentials {
+                    username = vsMavenUsername
+                    password = vsMavenPassword
+                }
+            }
+        }
+    }
+
+    publications {
+        register<MavenPublication>("vsMaven") {
+            from(components["java"])
+        }
+    }
+}
+
+fun String.execute(envp: Array<String>? = null, dir: File = projectDir): String {
+    val process = Runtime.getRuntime().exec(this, envp, dir)
+    return process.inputStream.reader().readText()
 }
